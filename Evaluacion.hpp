892ef2a27370efa93215a360d0eed755964ee120
Farrabs <44673600+Farrabs@users.noreply.github.com>
2018-11-08 00:55:42 +0100

    Update Evaluacion.hpp

--- Evaluacion.hpp
+++ Evaluacion.hpp
@@ -11,18 +11,20 @@ class Evaluacion
 private:
     int tam_exp; 
     
+    
 public:
       Evaluacion();
-      const char *expresion ;
+      char *expresion ;
       ~Evaluacion();
 
-      int Evaluar_ExpresionInfija(const char *exp);
-      int Evaluar_ExpresionInfija_2(const char* exp);
-      Cola ExpresionInfija_a_ExpresionPostfija (const char* exp);
+      int Evaluar_ExpresionInfija(char *exp);
+      int Evaluar_ExpresionInfija_2(char* exp);
+      Cola ExpresionInfija_a_ExpresionPostfija (char* exp);
       int Evaluar_ExpresionPosfija (Cola c);
-      bool es_Correcta(const char* exp);
-      void SacarTamano(const char* exp);
+      bool es_Correcta(char* exp);
+      void SacarTamano(char* exp);
       bool es_Num(char c);
+      int getTamano();
     
 };
 
