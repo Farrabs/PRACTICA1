1378fdee91c4898023ceeb63bf2157f296307d0f
Farrabs <44673600+Farrabs@users.noreply.github.com>
2018-11-07 19:19:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@
 
 int main(int argc, char **argv)
 {
-    Evaluacion e1;
+    Lista l1;
+    l1.InsertarIzq(8);
+    l1.InsertarIzq(9);
+    l1.InsertarIzq(1);
+    l1.Insertar(20,0);
+    l1.Mostrar();
+    l1.verUltimo();
+    l1.verPrimero();
+    cout << l1.Ver(3) << endl;
+    Evaluacion e1; 
 //cout << "El resultado es: " << e1.Evaluar_ExpresionInfija(e1.expresion);
-cout << "El resultado es: " << e1.expresion << " ==>" << e1.Evaluar_ExpresionInfija_2(e1.expresion);
+//cout << "El resultado es: " << e1.expresion << " ==>" << e1.Evaluar_ExpresionInfija_2(e1.expresion) << "\n";
 //cout << "Expresion: " << e1.expresion << " \n";
 //Cola c = e1.ExpresionInfija_a_ExpresionPostfija(e1.expresion);
-
-//cout << "El resultado es: " << e1.Evaluar_ExpresionPosfija(c);
+//cout << "El resultado es: " << e1.Evaluar_ExpresionPosfija(c) << " \n";
+//cout << "El resulado de correcto es: " << e1.es_Correcta(e1.expresion) << endl;
 }
