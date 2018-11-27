45d5bc270de65ec5a7b399fbcd6b9cc78e165d18
Farrabs <44673600+Farrabs@users.noreply.github.com>
2018-11-27 21:48:05 +0100

    Create Arbol.hpp

---
+++ Arbol.hpp
@@ -0,0 +1,35 @@
+#ifndef ARBOL_HPP
+#define ARBOL_HPP
+#include "NodoArbol.hpp"
+class Arbol
+{
+private:
+      pNodoArbol raiz, actual;
+      int contador, altura;
+      
+public:
+      Arbol();
+      ~Arbol();
+      void Podar(pNodoArbol &nodo);
+      void auxContador(pNodoArbol nodo);
+      void auxAltura(pNodoArbol nodo, int a);
+      void Mostrar(int &d);
+      bool buscarNodo (int v);
+      void insertarNodo (int v);
+      void borrarNodo (int v);
+      bool esArbolVacio();
+      bool esNodoVacio(pNodoArbol nodo);
+      bool esHoja(pNodoArbol nodo);
+      int numeroNodos();
+      int getAlturaArbol();
+      int getValorActual();
+      pNodoArbol getRaiz();
+      int getValorRaiz();
+      bool esRaiz(pNodoArbol nodo);
+      void inOrden(NodoArbol *nodo); 
+      void preOrden(NodoArbol *nodo);
+      void postOrden(NodoArbol *nodo);
+
+};
+
+#endif // ARBOL_HPP
