94efc64721491eb71d30f84b0372255ad5f47ba0
Farrabs <44673600+Farrabs@users.noreply.github.com>
2018-11-27 21:47:34 +0100

    Create NodoArbol.cpp

---
+++ NodoArbol.cpp
@@ -0,0 +1,12 @@
+#include "NodoArbol.hpp"
+
+NodoArbol::NodoArbol(int v, NodoArbol* der, NodoArbol* izq)
+{
+      valor = v;
+      derecha = der;
+      izquierda =izq;
+}
+
+NodoArbol::~NodoArbol()
+{
+}
