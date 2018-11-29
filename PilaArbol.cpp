f2d026b54590bd521850bca8f990040e35b84f73
Farrabs <44673600+Farrabs@users.noreply.github.com>
2018-11-29 22:44:00 +0100

    Update PilaArbol.cpp

--- PilaArbol.cpp
+++ PilaArbol.cpp
@@ -2,25 +2,33 @@
 
 PilaArbol::PilaArbol()
 {
+      ultimo = NULL;
 }
 
-void PilaArbol:: ApilarArbol(Arbol arbol){
+
+void PilaArbol:: ApilarArbol(pNodoArbol a){
       pNodoPilaArbol nuevo;
-      nuevo = new NodoPilaArbol(arbol, ultimo);
+      nuevo = new NodoPilaArbol (a, ultimo);
       ultimo = nuevo;
 }
 
-Arbol PilaArbol:: DesapilarArbol(){
-      pNodoPilaArbol nodo;
-      Arbol arbol ;
-      if(!ultimo){
-            return arbol;
-      }
-      nodo = ultimo;
-      ultimo = nodo->siguiente;
-      arbol = nodo->arbol;
-      delete nodo;
-      return arbol;
+pNodoArbol PilaArbol:: DesapilarArbol (){
+    pNodoPilaArbol nodo;
+    pNodoArbol a =NULL;
+    if (!ultimo){
+        return a;
+    }
+    nodo = ultimo;
+    ultimo = nodo->siguiente;
+    a = nodo->arbol;
+    
+    delete nodo;
+    return a;
+}
+
+pNodoArbol PilaArbol :: Cima(){
+    pNodoPilaArbol aux = ultimo;
+    return aux->arbol;
 }
 
 bool PilaArbol:: es_Vacia(){
@@ -28,7 +36,7 @@ bool PilaArbol:: es_Vacia(){
 }
 
 
+
 PilaArbol::~PilaArbol()
 {
 }
-
